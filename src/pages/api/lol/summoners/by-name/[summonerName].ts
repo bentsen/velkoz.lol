76432d48347544baa6aa4cb70ebba2c9669927a0
Mikkel Bentsen <bentsen405@gmail.com>
2022-09-30 19:03:09 +0200

    vercel 2

--- src/pages/api/lol/summoners/by-name/[summonerName].ts
+++ src/pages/api/lol/summoners/by-name/[summonerName].ts
@@ -31,7 +31,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) =>  {
                 puuid: newSummoner.puuid
             },
             data: {
-                id: newSummoner.id,
                 accountId: newSummoner.accountId,
                 puuid: newSummoner.puuid,
                 name: newSummoner.name,
