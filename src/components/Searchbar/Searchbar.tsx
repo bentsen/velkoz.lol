7fd9c873f728f81ff36862085baa7ee48f86b49a
Freddiiy <frederikgaller@live.dk>
2022-10-01 02:55:09 +0200

    some stuff

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -4,86 +4,122 @@ import {Combobox} from "@headlessui/react";
 import {IChampion} from "reksai/src/@types/champion";
 import {ddragon} from "reksai";
 import Image from "next/image"
+import {icon} from "@fortawesome/fontawesome-svg-core";
+import axios from "axios";
+import reksai from "reksai/src/Reksai";
+import DDragonVersions from "reksai/dist/clients/ddragon/DDragonVersions";
 
 
 const Searchbar = () => {
+	const [search, setSearch] = useState("");
 	const [selected, setSelected] = useState();
 	const [summoners, setSummoners] = useState<ISummoner[]>([]);
 	const [champions, setChampions] = useState<IChampion[]>([]);
-	const [search, setSearch] = useState("");
+	const [champAssets, setChampAssets] = useState(new Map<string, string>());
+	const [summonerIcons, setSummonerIcons] = useState(new Map<string, string>());
 
 
 	/** Handle champ data **/
 	useEffect(() => {
-		 async function handleChamps() {
+		const handleChamps = async () => {
 			const res = await ddragon.champion.getAll();
 			let champs = [];
 			for (let key in res.data) {
 				champs.push(res.data[key]);
+				await handleChampAsset(res.data[key].id)
 			}
 			setChampions(champs);
 		}
-
 		handleChamps();
-	}, [])
+
+		const handleChampAsset = async (champId: string) => {
+			const img = await ddragon.asset.champion(champId);
+			setChampAssets(champAssets.set(champId, img));
+		}
+	}, [champAssets])
+
+	useEffect(() => {
+		const handleSummonerIcons = async () => {
+			const x = Date.now();
+			const latest = await DDragonVersions.getLatestVersion();
+			const res = await axios.get(`https://ddragon.leagueoflegends.com/cdn/${latest}/data/en_US/profileicon.json`);
+			const data = await res.data.data;
+			let icons = [];
+			for (let key in data) {
+				icons.push(data[key]);
+				await handleIcon(data[key].id.toString());
+			}
+
+			const y = Date.now() - x;
+			console.log("DONE in: " + y)
+
+		}
+		handleSummonerIcons()
+
+		const handleIcon = async (iconId: string) => {
+			const res = await ddragon.asset.profileIcon(iconId);
+			setSummonerIcons(summonerIcons.set(iconId, res));
+		}
+
+	}, [summonerIcons])
 
 
 	const filteredSummoners = !search
 		? summoners
-		: summoners.filter((s) => s.name.toLowerCase().includes(search.toLowerCase()));
+		: summoners.filter((s) => s.name.toLowerCase().startsWith(search.toLowerCase()));
 
 	const filteredChamps = !search
 		? champions
-		: champions.filter((c) => c.name.toLowerCase().includes(search.toLowerCase()));
-
-	useEffect(() => {
-		filteredChamps.forEach((c) => console.log("ff: " + c.name))
-		console.log(search)
-	}, [search, champions, filteredChamps])
+		: champions.filter((c) => c.name.toLowerCase().startsWith(search.toLowerCase()));
 
 	return (
-		<div className={"relative w-full rounded-full max-w-4xl px-4"}>
-			<Combobox value={selected} onChange={setSelected}>
-				<Combobox.Input onChange={(e) => setSearch(e.target.value)}
-								className={`relative w-full max-w-4xl text-black p-4 text-2xl w-full max-w-4xl px-8 pr-16 ${search ? "rounded-t-3xl" : "rounded-3xl"}`}
-								placeholder={"Search Summoner og Champion..."}
-				/>
-				{search && (
-					<Combobox.Options className={"absolute max-h-70 w-full overflow-auto rounded-b-3xl bg-white"}>
-						{filteredSummoners.length > 0 && (
-							<>
-								<div className={"bg-gray-300"}>
-									<p className={"ml-5 text-gray-600"}>Summoner</p>
-								</div>
-								{filteredSummoners.map((s) => (
-									<Combobox.Option key={s.puuid} value={s}>
+		<>
+			<div className={"relative w-full rounded-full max-w-4xl"}>
+				<Combobox value={selected} onChange={setSelected}>
+					<div className={"w-full flex flex-row"}>
+						<Combobox.Input onChange={(e) => setSearch(e.target.value)}
+										className={`relative w-full text-black p-4 text-2xl w-full max-w-4xl px-8 pr-16 ${search ? "rounded-t-3xl" : "rounded-3xl"}`}
+										placeholder={"Search Summoner og Champion..."}
+						/>
+					</div>
+					{search && (
+						<Combobox.Options className={"absolute max-h-96 w-full overflow-auto rounded-b-3xl bg-white"}>
+							{filteredChamps.length > 0 && (
+								<>
+									<div className={"bg-gray-300"}>
+										<p className={"ml-5 text-gray-600"}>Champion</p>
+									</div>
+									{filteredChamps.map((c) => (
+										<Combobox.Option key={c.id} value={c}>
+											<UnifiedSearchOption img={champAssets.get(c.id)} name={c.name}/>
+										</Combobox.Option>
+									))}
+								</>
+							)}
 
+							<div className={"bg-gray-300"}>
+								<p className={"ml-5 text-gray-600"}>Summoner</p>
+							</div>
+							{filteredSummoners.length === 0 ? (
+								<UnifiedSearchOption img={summonerIcons.get("0")} name={search}/>
+							) : (
+								filteredSummoners.map((s) => (
+									<Combobox.Option key={s.puuid} value={s}
+													 className={"relative cursor-pointer py-2"}>
+										<UnifiedSearchOption img={summonerIcons.get(s.profileIconId.toString())} name={s.name}/>
 									</Combobox.Option>
-								))}
-							</>
-						)}
-
-						{filteredChamps.length > 0 && (
-							<>
-								<div className={"bg-gray-300"}>
-									<p className={"ml-5 text-gray-600"}>Champion</p>
-								</div>
-								{filteredChamps.map((c) => (
-									<Combobox.Option key={c.id} value={c}>
-										<UnifiedSearchOption img={c.image.sprite} name={c.name}/>
-									</Combobox.Option>
-								))}
-							</>
-						)}
-					</Combobox.Options>
-				)}
-			</Combobox>
-		</div>
+								))
+							)}
+						</Combobox.Options>
+					)}
+				</Combobox>
+			</div>
+		</>
 	)
 }
 
 interface UnifiedSearchOptionProps {
-	img: string,
+	img?: string,
 	region?: string,
 	name: string,
 	summonerLvl?: number
@@ -95,7 +131,9 @@ const UnifiedSearchOption = (props: UnifiedSearchOptionProps) => {
 			<div className={"bg-white"}>
 				<div className={"flex flex-row p-2"}>
 					<div className={"relative w-6 h-6"}>
-						<p>{props.img}</p>
+						{props.img && (
+							<Image src={props.img} alt={`${props.name} splash art`} layout={"fill"}/>
+						)}
 					</div>
 					<div className={"ml-2"}>
 						<p className={"text-gray-700 text-lg"}>{props.name}</p>
