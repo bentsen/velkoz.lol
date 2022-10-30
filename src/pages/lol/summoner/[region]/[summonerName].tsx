8575fbe724d90aeb719e627c10e283c0537b2761
Freddiiy <frederikgaller@live.dk>
2022-10-30 18:31:55 +0100

    possibÃe netflify typÃe fix for matches jeez

--- src/pages/lol/summoner/[region]/[summonerName].tsx
+++ src/pages/lol/summoner/[region]/[summonerName].tsx
@@ -190,10 +190,11 @@ const MatchHistory = ({
 	matches: TMatches | undefined;
 }) => {
 	if (!matches) return <div>Loading...</div>;
-	const sortedMatches = matches?.sort(
-		(a, b) =>
-			parseInt(b.info!.gameEndTimestamp) - parseInt(a.info!.gameEndTimestamp)
-	);
+
+	const sortedMatches = matches?.sort((a, b) => {
+		if (!a.info || !b.info) return -1;
+		return parseInt(b.info.gameEndTimestamp) - parseInt(a.info.gameEndTimestamp);
+	});
 
 	const times = 20;
 
